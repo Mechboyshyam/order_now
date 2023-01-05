c27e8a6bcbbf159119e2434ff71f5c2e1d13fc85
shyam <shyamkorade0@gmail.com>
2023-01-05 06:55:28 +0530

    Basic structure

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 import mongoose from "mongoose";
 import dotenv, { config } from 'dotenv'
-import express, { application } from "express";
+import express from "express";
+dotenv.config();
+
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGODB_URL , ()=>{
+    console.log('Connected to MongoDB');
+})
+
+// API starts here
+
+// API ends here
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
