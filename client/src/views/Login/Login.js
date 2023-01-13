1d42e8171f1212536ec9d79c731f95820fccf288
shyam <shyamkorade0@gmail.com>
2023-01-13 21:34:42 +0530

    signup format set

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -3,7 +3,7 @@ import React from "react";
 function Login(){
     return(
         <div>
-            <h2>Loginnnn</h2>
+            
         </div>
     )
 }
--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,9 +1,50 @@
 import React from "react";
+import './Signup.css';
 
-function Signup(){
-    return(
+function Signup() {
+    return (
         <div>
-            <h2>Signupppp</h2>
+            <h1 className="text-center">Signup</h1>
+            <div className="row">
+                <div className="col-md-6">
+
+                </div>
+
+                <div className="col-md-6">
+                    <div className="form-container">
+                        <form>
+                            <div className="form-group">
+                                <label htmlFor="name">Name</label>
+                                <input type='text' Classname='form-control' id='name'
+                                 placeholder='Enter name'  className="user-input"/>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="name">Email</label>
+                                <input type='email' Classname='form-control' id='name'
+                                 placeholder='Enter email'  className="user-input"/>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="name">Phone</label>
+                                <input type='text' Classname='form-control' id='name'
+                                 placeholder='Enter phone number'  className="user-input"/>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="name">Password</label>
+                                <input type='password' Classname='form-control' id='name'
+                                 placeholder='Enter password'  className="user-input"/>
+                            </div>
+
+                            <div>
+                                <button type="submit" className="signup-button">Signup</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
