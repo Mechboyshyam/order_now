4565e0484144e28e0a444aecd4e8e91b17d86543
shyam <shyamkorade0@gmail.com>
2023-01-18 21:14:20 +0530

    login page created

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import './Login.css'
 
-function Login(){
-    return(
+function Login() {
+
+    const [email, setEmail] = useState("")
+
+    const [password, setPassword] = useState("")
+
+
+    async function loginUser() {
+        const response = await axios.post('/signup', {
+
+            email: email,
+
+            password: password,
+
+        })
+
+        console.log(response.data)
+        if (response.data.success) {
+            alert(response.data.message)
+            window.location.href = '/login'
+        }
+
+        else {
+            alert(response.data.message)
+
+            setEmail('')
+
+            setPassword('')
+
+        }
+    }
+
+    return (
         <div>
-            
+            <h1 className="text-center">Signup</h1>
+            <div className="row">
+                <div className="col-md-6">
+
+                </div>
+
+                <div className="col-md-6">
+                    <div className="form-container">
+                        <form>
+
+
+                            <div className="form-group">
+                                <label htmlFor="name">Email</label>
+                                <input type='email' Classname='form-control' id='name'
+                                    placeholder='Enter email' className="user-input"
+                                    value={email} onChange={(e) => setEmail(e.target.value)} />
+                            </div>
+
+
+
+                            <div className="form-group">
+                                <label htmlFor="name">Password</label>
+                                <input type='password' Classname='form-control' id='name'
+                                    placeholder='Enter password' className="user-input"
+                                    value={password} onChange={(e) => setPassword(e.target.value)} />
+                            </div>
+
+                            <div>
+                                <button type="submit" className="signup-button" onClick={loginUser}>Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,52 +1,88 @@
-import React from "react";
-import './Signup.css';
+import React, { useState } from "react";
+import axios from "axios";
+import './Signup.css'
 
 function Signup() {
-    return (
-        <div>
-            <h1 className="text-center">Signup</h1>
-            <div className="row">
-                <div className="col-md-6">
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phone, setPhone] = useState("")
+    const [password, setPassword] = useState("")
+    const [role, setRole] = useState("user")
 
-                </div>
+    async function signupUser() {
+        const response = await axios.post('/signup', {
+            name: name,
+            email: email,
+            phone: phone,
+            password: password,
+            role: role
+        })
 
-                <div className="col-md-6">
-                    <div className="form-container">
-                        <form>
-                            <div className="form-group">
-                                <label htmlFor="name">Name</label>
-                                <input type='text' Classname='form-control' id='name'
-                                 placeholder='Enter name'  className="user-input"/>
-                            </div>
+        console.log(response.data)
+        if (response.data.success) {
+            alert(response.data.message)
+            window.location.href = '/login'
+        }
 
-                            <div className="form-group">
-                                <label htmlFor="name">Email</label>
-                                <input type='email' Classname='form-control' id='name'
-                                 placeholder='Enter email'  className="user-input"/>
-                            </div>
+        else {
+            alert(response.data.message)
+            setName('')
+            setEmail('')
+            setPhone('')
+            setPassword('')
 
-                            <div className="form-group">
-                                <label htmlFor="name">Phone</label>
-                                <input type='text' Classname='form-control' id='name'
-                                 placeholder='Enter phone number'  className="user-input"/>
-                            </div>
+        }
+    }
 
-                            <div className="form-group">
-                                <label htmlFor="name">Password</label>
-                                <input type='password' Classname='form-control' id='name'
-                                 placeholder='Enter password'  className="user-input"/>
-                            </div>
-
-                            <div>
-                                <button type="submit" className="signup-button">Signup</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+return (
+    <div>
+        <h1 className="text-center">Signup</h1>
+        <div className="row">
+            <div className="col-md-6">
 
             </div>
+
+            <div className="col-md-6">
+                <div className="form-container">
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="name">Full Name</label>
+                            <input type='text' Classname='form-control' id='name'
+                                placeholder='Enter name' className="user-input"
+                                value={name} onChange={(e) => setName(e.target.value)} />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="name">Email</label>
+                            <input type='email' Classname='form-control' id='name'
+                                placeholder='Enter email' className="user-input"
+                                value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="name">Phone</label>
+                            <input type='text' Classname='form-control' id='name'
+                                placeholder='Enter phone number' className="user-input"
+                                value={phone} onChange={(e) => setPhone(e.target.value)} />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="name">Password</label>
+                            <input type='password' Classname='form-control' id='name'
+                                placeholder='Enter password' className="user-input"
+                                value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+
+                        <div>
+                            <button type="submit" className="signup-button" onClick={signupUser}>Signup</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
         </div>
-    )
+    </div>
+)
 }
 
 export default Signup;
