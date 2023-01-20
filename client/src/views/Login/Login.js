8f9280ffa308e032ca194d3762b51eee10cf0693
shyam <shyamkorade0@gmail.com>
2023-01-20 05:43:29 +0530

    sweet alert applied

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import swal from 'sweetalert';
 import './Login.css'
 import {currentUser} from '../../util/currentUser.js';
 
@@ -11,7 +12,7 @@ function Login() {
         if(currentUser){
             window.location.href= "/"
         }
-    })
+    }, [])
 
     async function loginUser() {
         const response = await axios.post('/login', {
@@ -22,15 +23,25 @@ function Login() {
 
         console.log(response.data)
         if (response.data.success) {
-            alert(response.data.message)
+            await swal({
+                title: "Success",
+                text: response.data.message,
+                icon: "success",
+                button: "Aww yiss!"});
             localStorage.setItem('currentUser', JSON.stringify(response.data.data));
             window.location.href='/'
         }
 
         else {
-            alert(response.data.message)
+            await swal({
+                title: "Error",
+                text: response.data.message,
+                icon: "error",
+                button: "Try Again!",
+              });
             setEmail('')
             setPassword('')
+            localStorage.removeItem('currentUser');
         }
     }
 
@@ -47,24 +58,24 @@ function Login() {
                         <form>
 
 
-                            <div className="form-group">
+                            <div>
                                 <label htmlFor="name">Email</label>
-                                <input type='email' Classname='form-control' id='name'
+                                <input type='email' id='email'
                                     placeholder='Enter email' className="user-input"
                                     value={email} onChange={(e) => setEmail(e.target.value)} />
                             </div>
 
 
 
-                            <div className="form-group">
+                            <div>
                                 <label htmlFor="name">Password</label>
-                                <input type='password' Classname='form-control' id='name'
+                                <input type='password' id='name'
                                     placeholder='Enter password' className="user-input"
                                     value={password} onChange={(e) => setPassword(e.target.value)} />
                             </div>
 
                             <div>
-                                <button type="submit" className="signup-button" onClick={loginUser}>Login</button>
+                                <button type="button" className="login-button" onClick={loginUser}>Login</button>
                             </div>
                         </form>
                     </div>
--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import swal from 'sweetalert';
 import './Signup.css'
 import {currentUser} from '../../util/currentUser.js';
 
