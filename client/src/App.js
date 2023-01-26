54d4b9f6ddd5800ea015535b2918144113c9d1ff
shyam <shyamkorade0@gmail.com>
2023-01-26 21:13:12 +0530

    code rechacked

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './views/Home/Home.js';
 import Signup from './views/Signup/Signup.js';
 import Login from './views/Login/Login.js';
+import BookTable from "./views/BookTable/BookTable.js";
+import MyOrders from "./views/MyOrders/MyOrders.js";
 
 
 function App(){
@@ -14,6 +16,8 @@ function App(){
           <Route path="/" element={< Home />} />
           <Route path="/login" element={< Login />} />
           <Route path="/signup" element={< Signup />} />
+          <Route path="/booktable" element={< BookTable />} />
+          <Route path="/myorders" element={< MyOrders />} />
         </Routes>
       </BrowserRouter>
     </div>
